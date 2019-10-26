b5e78b5b9032e1e53de04ce3f3f5ad64dde16dea
sarkaromar <ausantemonir@gmial.com>
2019-10-26 17:17:03 +0600

    Design

---
+++ README.md
@@ -0,0 +1,53 @@
+# CMS Web Desing
+
+### Feature
+* Comming Soon
+
+### Tech
+* [HTML5]
+* [CSS3]
+* [JS] - here's some code! :+1:
+* [jQuery] - Plugins And here's some code! :+1:
+* [Bootstrap 4] - great UI boilerplate for modern web apps
+* [Gulp] - the streaming build system
+* [Graphics] - Photoshop & illustrator
+* [Editor] - Sublime & VSCode
+
+### How to install
+---
+Pre install
+Node and npm
+
+Make sure that you've installed Node and npm before attempting to install gulp.
+```sh
+$ node -v
+$ npm -v
+```
+or check the url if you not understand  
+```sh
+https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md
+```
+Then
+
+Clone with HTTPS 
+Use Git or checkout with SVN using the web URL.
+
+```sh
+https://sarkarmonir@bitbucket.org/sarkarmonir/sms.git
+```
+
+```sh
+cd sms/
+$ npm install
+```
+
+Install a version of the package matching the specified version range. This will follow the same rules for resolving dependencies described in package.json.
+
+Run:
+```sh
+$ gulp
+```
+
+License
+---
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+// main require var
+var gulp = require('gulp');
+// others require plugins
+var sass          = require('gulp-sass'),
+    watch         = require('gulp-watch'),
+    browserSync   = require('browser-sync').create(),
+    useref        = require('gulp-useref'),
+    uglify        = require('gulp-uglify'),
+    plumber       = require('gulp-plumber'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    cssmin        = require('gulp-cssmin'),
+    rename        = require('gulp-rename'),
+    gulpIf        = require('gulp-if'),
+    htmlmin       = require('gulp-html-minifier'),
+    livereload    = require('gulp-livereload'),
+    imagemin      = require('gulp-imagemin');
+    
+
+    // serve start
+    gulp.task('browserSync', function() {
+      browserSync.init({
+        server: {
+          baseDir: 'app'
+        },
+      })
+    });
+
+    // vendors taks
+    gulp.task('vendors', function() {
+    // bootstrap
+      gulp.src('node_modules/bootstrap/dist/**/*.min.css')
+      .pipe(gulp.dest('app/vendors'));
+      gulp.src('node_modules/bootstrap/dist/**/*.min.js')
+      .pipe(gulp.dest('app/vendors'));
+      //jquery
+      gulp.src('node_modules/jquery/dist/*.min.js')
+      .pipe(gulp.dest('app/vendors/jquery'));
+      //animate.css
+      gulp.src('node_modules/animate.css/*.min.css')
+      .pipe(gulp.dest('app/vendors/css'));
+      //wow.js
+      gulp.src('node_modules/wow.js/dist/*.min.js')
+      .pipe(gulp.dest('app/vendors/js'));
+
+    });
+
+    // Sass task
+    gulp.task('sass', function(){
+      return gulp.src('app/sass/*.scss')
+      .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+          browsers: ['Android >= 2.1',
+                     'Chrome >= 21',
+                     'Edge >= 12',
+                     'Explorer >= 7',
+                     'Firefox >= 17',
+                     'Opera >= 12.1',
+                     'Safari >= 6.0'],
+          cascade: false}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
+    });
+
+    // images task
+    // gulp.task('image', () =>
+    //   gulp.src('app/images/*')
+    //     .pipe(imagemin())
+    //     .pipe(gulp.dest('app/img'))
+    // );
+    
+    // watch task
+    gulp.task('watch:sass', function () {
+      gulp.watch('app/sass/*.scss', ['sass']);
+    });
+
+    // gulp build task
+    gulp.task('build', function() {
+      // // bootstrap
+      // gulp.src('node_modules/bootstrap/dist/**/*.min.css')
+      // .pipe(gulp.dest('dist/vendors'));
+      // gulp.src('node_modules/bootstrap/dist/**/*.min.js')
+      // .pipe(gulp.dest('dist/vendors'));
+      // //jquery
+      // gulp.src('node_modules/jquery/dist/*.min.js')
+      // .pipe(gulp.dest('dist/vendors/jquery'));
+       
+      gulp.src('app/vendors/**')
+      .pipe(gulp.dest('dist/vendors'));
+      //custom js
+      gulp.src('app/js/*.js')
+      .pipe(gulp.dest('dist/js'));
+      //css
+      gulp.src('app/css/*.min.css')
+      .pipe(gulp.dest('dist/css'))
+      //vendor css
+      gulp.src('app/css/vendor/*.css')
+      .pipe(gulp.dest('dist/css'));
+      //html
+      gulp.src('app/*.html')
+      //.pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist'))
+      //images 
+      gulp.src('app/images/**')
+      .pipe(imagemin())
+      .pipe(gulp.dest('dist/images'))
+    });
+
+// default task
+gulp.task('default', ['sass', 'watch:sass', 'vendors', 'browserSync']);
